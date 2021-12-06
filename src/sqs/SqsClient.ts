a34a3631459bc9385b875edb07f7ba1c6f774b8f
Calvin Matthews <calvin.matthews@cmatts.co.uk>
2021-12-06 12:17:15 +0000

    Implemented sqs examples using extended bucket

---
+++ src/sqs/SqsClient.ts
@@ -0,0 +1,162 @@
+import { SQS } from 'aws-sdk';
+import {
+    ClientConfiguration,
+    CreateQueueRequest,
+    GetQueueUrlRequest,
+    GetQueueUrlResult,
+    Message,
+    PurgeQueueRequest,
+    ReceiveMessageRequest,
+    ReceiveMessageResult,
+    SendMessageBatchRequest,
+    SendMessageBatchRequestEntry,
+    SendMessageRequest,
+} from 'aws-sdk/clients/sqs';
+import { v4 as getUuid } from 'uuid';
+import S3Client from '@/s3/S3Client';
+
+export default class SqsClient {
+    private static readonly BATCH_SIZE: number = 25;
+    private static readonly MAX_MESSAGE_SIZE: number = 256000;
+    private static readonly LARGE_PAYLOAD_ID: string = 'software.amazon.payloadoffloading.PayloadS3Pointer';
+    private static readonly LARGE_MESSAGE_EXP: RegExp = new RegExp(`^\\["${SqsClient.LARGE_PAYLOAD_ID}",(.*)\\]$`);
+    private readonly extendedClientBucket: string | undefined;
+
+    private client: SQS | undefined;
+
+    public constructor(extendedClientBucket: string) {
+        this.extendedClientBucket = extendedClientBucket;
+    }
+
+    private getSqsClient(): SQS {
+        if (this.client) {
+            return this.client;
+        }
+
+        this.client = new SQS(SqsClient.getOptions());
+        return this.client;
+    }
+
+    private static getOptions(): ClientConfiguration {
+        if (process.env.AWS_LOCAL_ENDPOINT) {
+            return {
+                endpoint: process.env.AWS_LOCAL_ENDPOINT,
+                region: process.env.AWS_REGION,
+            };
+        }
+        return {};
+    }
+
+    public async createQueue(queueName: string): Promise<void> {
+        const params: CreateQueueRequest = {
+            QueueName: queueName,
+        };
+        await this.getSqsClient().createQueue(params).promise();
+    }
+
+    public async purgeQueue(queueName: string): Promise<void> {
+        const params: PurgeQueueRequest = {
+            QueueUrl: await this.getQueueUrl(queueName),
+        };
+        await this.getSqsClient().purgeQueue(params).promise();
+    }
+
+    public async sendToQueue(queueName: string, message: string): Promise<void> {
+        const params: SendMessageRequest = {
+            QueueUrl: await this.getQueueUrl(queueName),
+            MessageBody: message,
+        };
+        await this.getSqsClient().sendMessage(params).promise();
+    }
+
+    public async readFromQueue(queueName: string): Promise<string[]> {
+        const params: ReceiveMessageRequest = {
+            QueueUrl: await this.getQueueUrl(queueName),
+        };
+        const response: ReceiveMessageResult = await this.getSqsClient().receiveMessage(params).promise();
+
+        return !response.Messages ? [] : response.Messages.map((m: Message) => <string>m.Body);
+    }
+
+    public async sendToExtendedQueue(queueName: string, messages: string[]): Promise<void> {
+        for (let i = 0; i < messages.length; i += SqsClient.BATCH_SIZE) {
+            const batch: SendMessageBatchRequestEntry[] = await Promise.all(
+                messages
+                    .slice(i, i + SqsClient.BATCH_SIZE)
+                    .map(async (message: string) => await this.messagePayload(message)),
+            );
+
+            const params: SendMessageBatchRequest = {
+                QueueUrl: await this.getQueueUrl(queueName),
+                Entries: batch,
+            };
+            await this.getSqsClient().sendMessageBatch(params).promise();
+        }
+    }
+
+    private async messagePayload(message: string): Promise<SendMessageBatchRequestEntry> {
+        let uuid: string = getUuid();
+        let messageBody: string = message;
+
+        if (Buffer.byteLength(message, 'utf8') > SqsClient.MAX_MESSAGE_SIZE) {
+            uuid = await this.storeOriginalMessage(message);
+            messageBody = this.largeMessagePayload(uuid);
+        }
+
+        return {
+            Id: uuid,
+            MessageBody: messageBody,
+        };
+    }
+
+    private largeMessagePayload(uuid: string): string {
+        return `["${SqsClient.LARGE_PAYLOAD_ID}",{"s3BucketName":"${this.extendedClientBucket}","s3Key":"${uuid}"}]`;
+    }
+
+    public async readFromExtendedQueue(queueName: string): Promise<string[]> {
+        const messages = await this.readFromQueue(queueName);
+        return Promise.all(messages.map(async (message: string) => await this.toOriginalMessage(message)));
+    }
+
+    public async toOriginalMessage(message: string): Promise<string> {
+        const match: RegExpExecArray | null = SqsClient.LARGE_MESSAGE_EXP.exec(message);
+        if (!match) {
+            return message;
+        }
+
+        const largeMessagePayload = JSON.parse(match[1]);
+        const s3Url = SqsClient.getLargeMessageS3Url(largeMessagePayload.s3BucketName, largeMessagePayload.s3Key);
+        return await S3Client.readFromBucket(s3Url);
+    }
+
+    // public async deleteOriginalMessage(message: string): Promise<void> {
+    //     // const largeMessagePayload = JSON.parse(match[1]);
+    //     // const s3Url = SqsClient.getLargeMessageS3Url(largeMessagePayload.s3BucketName, largeMessagePayload.s3Key);
+    //     // await S3Client.delete(s3Url);
+    // }
+
+    public async storeOriginalMessage(message: string): Promise<string> {
+        if (!this.extendedClientBucket) {
+            throw new Error('Extended client bucket is not defined.');
+        }
+        const uuid: string = getUuid();
+        const url: string = SqsClient.getLargeMessageS3Url(this.extendedClientBucket, uuid);
+        await S3Client.writeToBucket(url, message);
+        return uuid;
+    }
+
+    private static getLargeMessageS3Url(bucket: string, uuid: string): string {
+        return `s3://${bucket}/${uuid}`;
+    }
+
+    private async getQueueUrl(queueName: string): Promise<string> {
+        const params: GetQueueUrlRequest = {
+            QueueName: queueName,
+        };
+        const response: GetQueueUrlResult = await this.getSqsClient().getQueueUrl(params).promise();
+        if (!response.QueueUrl) {
+            throw new Error('Can not get queue url');
+        }
+        return response.QueueUrl;
+    }
+}
---
+++ test/sqs/SqsClient.test.ts
@@ -0,0 +1,115 @@
+import LocalStackContainer from '../jestcontainers/LocalStackContainer';
+import LocalStackService from '../jestcontainers/LocalStackService';
+import SqsClient from '@/sqs/SqsClient';
+import S3Client from '@/s3/S3Client';
+
+describe('sqs client test', () => {
+    const TEST_QUEUE_BUCKET = 'my-queue-bucket';
+    const TEST_QUEUE = 'myQueue';
+    const TEST_MESSAGE = 'A test message';
+    const ONE_HUNDRED_MILLISECONDS = 100;
+    const FIVE_SECONDS = 5000;
+    const EXTENDED_MESSAGE_PAYLOAD =
+        '\\["software.amazon.payloadoffloading.PayloadS3Pointer",{"s3BucketName":"my-queue-bucket","s3Key":".*"}\\]';
+
+    let localStackContainer: LocalStackContainer | undefined;
+    let sqsClient: SqsClient | undefined;
+
+    beforeAll(async () => {
+        localStackContainer = await LocalStackContainer.create([LocalStackService.SQS, LocalStackService.S3]);
+
+        process.env.AWS_REGION = 'us-east-1';
+        process.env.AWS_ACCESS_KEY = 'access-key';
+        process.env.AWS_SECRET_ACCESS_KEY = 'secret-key';
+        process.env.AWS_LOCAL_ENDPOINT = localStackContainer.getEndpoint();
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+        await S3Client.createBucket(TEST_QUEUE_BUCKET);
+        await getSqsClient().createQueue(TEST_QUEUE);
+    });
+
+    afterAll(async () => {
+        if (localStackContainer) {
+            await localStackContainer.destroy();
+        }
+    });
+
+    beforeEach(async () => {
+        await getSqsClient().purgeQueue(TEST_QUEUE);
+    });
+
+    it('should Send To Queue', async () => {
+        await getSqsClient().sendToQueue(TEST_QUEUE, TEST_MESSAGE);
+
+        const receivedMessages: string[] = await getSqsClient().readFromQueue(TEST_QUEUE);
+        expect(receivedMessages).toHaveLength(1);
+        expect(receivedMessages[0]).toBe(TEST_MESSAGE);
+    });
+
+    it('should Send Simple Message To Extended Queue', async () => {
+        await getSqsClient().sendToExtendedQueue(TEST_QUEUE, [TEST_MESSAGE]);
+
+        const receivedMessages: string[] = await getSqsClient().readFromQueue(TEST_QUEUE);
+        expect(receivedMessages).toHaveLength(1);
+        expect(receivedMessages[0]).toBe(TEST_MESSAGE);
+    });
+
+    it('should read Simple Message from Extended Queue', async () => {
+        await getSqsClient().sendToQueue(TEST_QUEUE, TEST_MESSAGE);
+
+        const receivedMessages: string[] = await getSqsClient().readFromExtendedQueue(TEST_QUEUE);
+        expect(receivedMessages).toHaveLength(1);
+        expect(receivedMessages[0]).toBe(TEST_MESSAGE);
+    });
+
+    it('should Send LargeMessage To Extended Queue', async () => {
+        const largeMessage: string = 'X'.repeat(257 * 1024);
+        await getSqsClient().sendToExtendedQueue(TEST_QUEUE, [largeMessage]);
+
+        const receivedMessages: string[] = await getSqsClient().readFromQueue(TEST_QUEUE);
+        expect(receivedMessages).toHaveLength(1);
+        expect(receivedMessages[0]).toEqual(expect.stringMatching(EXTENDED_MESSAGE_PAYLOAD));
+    });
+
+    it('should Receive Large Message From Extended Queue', async () => {
+        const largeMessage: string = 'X'.repeat(257 * 1024);
+        await getSqsClient().sendToExtendedQueue(TEST_QUEUE, [largeMessage]);
+
+        const receivedMessages: string[] = await getSqsClient().readFromExtendedQueue(TEST_QUEUE);
+        expect(receivedMessages).toHaveLength(1);
+        expect(receivedMessages[0]).toEqual(largeMessage);
+    });
+
+    it('should Send Big Message Batch To Extended Queue', async () => {
+        const largeMessage: string = 'X'.repeat(250 * 1024);
+        const messageBatch: string[] = [largeMessage, largeMessage, largeMessage];
+        await getSqsClient().sendToExtendedQueue(TEST_QUEUE, messageBatch);
+
+        const receivedMessages: string[] = await retrieveMessagesFromSqs(3);
+        expect(receivedMessages[0]).toEqual(largeMessage);
+    });
+
+    const retrieveMessagesFromSqs = async (numberOfRecords: number): Promise<string[]> => {
+        let messages: string[] = [];
+        let timeOut: number = FIVE_SECONDS;
+
+        while (messages.length < numberOfRecords) {
+            if (timeOut <= 0) {
+                throw new Error('Timed out waiting for messages');
+            }
+
+            await new Promise((r) => setTimeout(r, ONE_HUNDRED_MILLISECONDS));
+            timeOut -= ONE_HUNDRED_MILLISECONDS;
+            const items: string[] = await getSqsClient().readFromQueue(TEST_QUEUE);
+            messages = messages.concat(items);
+        }
+        return messages;
+    };
+
+    const getSqsClient = (): SqsClient => {
+        if (!sqsClient) {
+            sqsClient = new SqsClient(TEST_QUEUE_BUCKET);
+        }
+        return sqsClient;
+    };
+});
